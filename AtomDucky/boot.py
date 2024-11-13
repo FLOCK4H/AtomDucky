f11457acc1329d23858b3ec846463e4165b77a5d
FLOCK4H <flock4h@gmail.com>
2024-11-13 22:34:12 +0100

    Update boot.py

--- AtomDucky/boot.py
+++ AtomDucky/boot.py
@@ -1,5 +1,7 @@
 import storage
 import supervisor
 
+# Uncomment the line below to hide usb drive
+#storage.disable_usb_drive()
 storage.remount('/', disable_concurrent_write_protection=True)
 supervisor.runtime.autoreload = False
